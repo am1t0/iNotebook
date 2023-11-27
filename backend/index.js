b8402a6d9fb5d1edfecab66810e54ceefb971dad
am1t0 <amiitt43@gmail.com>
2023-11-27 14:46:49 +0530

    models and routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const connectToMongo = require('./db');
+const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,6 +8,9 @@ connectToMongo()
 app.get('/', (req, res) => {
   res.send('Hello Amit!')
 })
+// Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
