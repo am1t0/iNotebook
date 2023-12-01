76767c9f95f19bc9bef57e8e0fe22219bcde294e
am1t0 <amiitt43@gmail.com>
2023-12-01 18:02:18 +0530

    thunderClient Collection,Refactoring, Email uniquenes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const app = express()
-const port = 3000
+const port = 5000
 connectToMongo()
 
 app.use(express.json())      // middleware for using body of request
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,23 +4,41 @@ const router = express.Router();
 const User = require('../models/user')
 
 
-router.post('/',[
+router.post('/createUser',//array of validations
+[
     body('name','Enter a valid name').isLength({min:3}),
     body('email','enter a valid email').isEmail(),
     body('password').isLength({min:8})
-],(req,res)=>{
+],
+async (req,res)=>{
+
+  // if there are errors return the errors
     const result = validationResult(req);
   if (!result.isEmpty()) {
     return res.send({ errors: result.array() });
   }
 
-  User.create({
-    name: req.body.name,
-    password: req.body.password,
-    email:req.body.email
-  }).then(user => res.json(user))
-  .catch(err=>{console.log(\err)
-   res.json({error:"please enter unique value for email",message:err.message})});
+  // check whether user with the given email already exists
+  try{
+ // using async - await here as many methods are asynchronous i.e. "findOne"
+    let user = await User.findOne({email:req.body.email})
+  
+    if(user){return res.json({error:"Sorry! a user with this email already exists"})}
+  
+    // Creating a new user from request body
+      user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email:req.body.email })
+  
+      res.json(user)
+  }
+  // catching error if there is any in the server
+  catch(error){
+    console.error(error.message);
+    res.status(500).send("Server error")
+  }
+  
 })
 
 module.exports = router
